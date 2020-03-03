f7e96dfac76a704b01a13efbd75cacd2d1fa4d1d
David Morales <git@damorales.com>
2020-03-02 22:41:59 -0600

    Remove puppeteer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-const puppeteer = require('puppeteer');
 const nodemailer = require('nodemailer');
+const axios = require('axios').default;
+const axiosCookieJarSupport = require('axios-cookiejar-support').default;
+const tough = require('tough-cookie');
+const querystring = require('querystring');
 const fs = require('fs');
+
 const conf = require('./conf.json');
 
 const STORE_FILE = 'src/storage.json';
+
+const cookieJar = new tough.CookieJar();
 
 const transport = nodemailer.createTransport({
   host: conf.smtp_host,
@@ -21,39 +27,60 @@ const message = {
   text: ''
 };
 
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: 'https://www.yotepresto.com/',
+  headers: {
+    'Accept': '*/*;q=0.5, text/javascript, application/javascript, application/ecmascript, application/x-ecmascript',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'
+  },
+});
+
+axiosCookieJarSupport(instance);
+// instance.defaults.jar = cookieJar;
+instance.defaults.jar = true;
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  try {
+    const loginPayload = {
+      "uf8": "✓",
+      "sessions[email]": conf.email,
+      "sessions[password]": conf.password,
+      "commit": "Iniciar Sesión"
+    };
 
-  await page.goto('https://www.yotepresto.com/sesion-inversionistas');
+    const login = await instance.post('sign_in', {
+      data: querystring.stringify(loginPayload),
+    });
 
-  await page.type('input#sessions_email', conf.email);
-  await page.type('input#sessions_password', conf.password);
-  await page.keyboard.press('Enter');
+    const inFunding = await instance.get('user/dashboard_widgets/in_funding');
 
-  await page.waitForNavigation();
+    // console.log(inFunding.data);
 
-  await page.goto('https://www.yotepresto.com/user/dashboard_widgets/in_funding');
-  const html = await page.content();
+    const exp = /<span>(\d+)<\/span>/i;
+    const match = inFunding.data.match(exp);
+    const reqCount = +match[1];
 
-  const exp = /\&gt;(\d+)&lt;/i;
-  const match = html.match(exp);
-  const reqCount = +match[1];
+    let prev = {};
+    if (fs.existsSync(STORE_FILE)) {
+      const storage = fs.readFileSync(STORE_FILE);
+      prev = JSON.parse(storage);
+    }
+    const prevReqCount = prev.reqCount || 0;
 
-  let prev = {};
-  if (fs.existsSync(STORE_FILE)) {
-    const storage = fs.readFileSync(STORE_FILE);
-    prev = JSON.parse(storage);
+    if (reqCount > prevReqCount) {
+      console.log('Notify...');
+      const text = `There are ${reqCount} new requisitions.`;
+      console.log(text);
+      await transport.sendMail({...message, text});
+    } else {
+      console.log('No new requisitions.');
+    }
+
+    fs.writeFileSync(STORE_FILE, JSON.stringify({reqCount}));
+
+  } catch (error) {
+    console.error(error);
   }
-
-  if (reqCount > prev.reqCount) {
-    console.log('Notify...');
-    const text = `The are ${reqCount} new requisitions.`;
-    console.log(text);
-    await transport.sendMail({...message, text});
-  }
-
-  fs.writeFileSync(STORE_FILE, JSON.stringify({reqCount}));
-  await browser.close();
 })();
